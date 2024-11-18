173523c6066cbcf4fccf7c13b548074e3f34f5d4
yindaheng98 <yindaheng98@gmail.com>
2024-11-18 11:26:57 -0800

    no cache_features in offline cotracker3

--- dot/models/shelf/cotracker3.py
+++ dot/models/shelf/cotracker3.py
@@ -13,4 +13,4 @@ class CoTracker3(nn.Module):
         )
 
     def forward(self, video, queries, backward_tracking, cache_features=False):
-        return self.model(video, queries=queries, backward_tracking=backward_tracking, cache_features=cache_features)
+        return self.model(video, queries=queries, backward_tracking=backward_tracking)
